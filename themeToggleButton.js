edcc8aaa4b610dfd9b419b4410ca8b170a639a5e
ChampyTech <66367029+ChampyTech@users.noreply.github.com>
2025-07-31 14:16:20 +0200

    Edit icons for themeToggleButton

--- themeToggleButton.js
+++ themeToggleButton.js
@@ -4,12 +4,12 @@ const themeToggleButton = document.getElementById('themeToggleButton');
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
     document.body.classList.add('dark');
-    themeToggleButton.textContent = '☀️ Light mode';
+    themeToggleButton.textContent = '<i class="fa-solid fa-sun"></i> Light mode';
 };
 
 // Toggle and save to click
 themeToggleButton.addEventListener('click', () => {
     const isDark = document.body.classList.toggle('dark');
     localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    themeToggleButton.textContent = isDark ? '☀️ Light mode' : '🌙 Dark mode';
+    themeToggleButton.textContent = isDark ? '<i class="fa-solid fa-sun"></i> Light mode' : '<i class="fa-solid fa-moon"></i> Dark mode';
 });
